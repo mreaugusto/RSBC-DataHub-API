bd7d6e6ba082e58c12bbd214d5cd3bd5838a4460
Jonathan Longe <jlonge@corebox.net>
2022-08-04 14:36:39 -0700

    update web_app to v0.6.1

---
+++ web_app/dist/precache-manifest.cdfbdc5decfbc7d520808c89c87d535f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a1d1387d63bfa8f038a3563072d61e5",
+    "url": "/roadside-forms/assets/BCID_RoadSafetyBC_logo_transparent.png"
+  },
+  {
+    "revision": "5ce4da386a4eba0d6dab42b7f2bccf02",
+    "url": "/roadside-forms/assets/MV2634_102018_driver.png"
+  },
+  {
+    "revision": "99649d1f1ffade92e5110771bf0a69e3",
+    "url": "/roadside-forms/assets/MV2634_102018_police.png"
+  },
+  {
+    "revision": "84c56b7303097ba856dad1783b9a0311",
+    "url": "/roadside-forms/assets/MV2721_201502.png"
+  },
+  {
+    "revision": "7bbb24a042f52a431a88bce5a4caad00",
+    "url": "/roadside-forms/assets/MV2721_201502_appeal.png"
+  },
+  {
+    "revision": "1806cf03f5f0fb23651e55de8e313de8",
+    "url": "/roadside-forms/assets/MV2722_201502.png"
+  },
+  {
+    "revision": "d7475b61cb2abb2145ba93f320d3f3e7",
+    "url": "/roadside-forms/assets/MV2723_201602.png"
+  },
+  {
+    "revision": "3ffbcc270befb546fe7de5d9799621ce",
+    "url": "/roadside-forms/assets/MV2906_102018.png"
+  },
+  {
+    "revision": "6ad5ec0bdd5be97db0dd",
+    "url": "/roadside-forms/css/app.93821d62.css"
+  },
+  {
+    "revision": "fd32f2c8f256b14f306a",
+    "url": "/roadside-forms/css/chunk-vendors.ac5b10c9.css"
+  },
+  {
+    "revision": "73a897b7660443eac7b304450e37fe20",
+    "url": "/roadside-forms/index.html"
+  },
+  {
+    "revision": "6ad5ec0bdd5be97db0dd",
+    "url": "/roadside-forms/js/app.60907730.js"
+  },
+  {
+    "revision": "fd32f2c8f256b14f306a",
+    "url": "/roadside-forms/js/chunk-vendors.71f832db.js"
+  },
+  {
+    "revision": "d614e0fcb8d493b145b2ccefdb520a31",
+    "url": "/roadside-forms/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/roadside-forms/robots.txt"
+  }
+]);
--- web_app/dist/service-worker.js
+++ web_app/dist/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/roadside-forms/precache-manifest.d5e0114c8238dde72cce35714580cc90.js", "/roadside-forms/workbox-v4.3.1/workbox-sw.js");
+importScripts("/roadside-forms/precache-manifest.cdfbdc5decfbc7d520808c89c87d535f.js", "/roadside-forms/workbox-v4.3.1/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/roadside-forms/workbox-v4.3.1"});
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {
