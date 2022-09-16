c1fa678f3f05b1a4a1dd670eb30c0ea946d1d478
Sukhbir Singh Ghotra <sukhbirghotra@gmail.com>
2022-09-16 13:51:22 -0400

    release/s166 to v0.7.6

--- web_app/dist/service-worker.js
+++ web_app/dist/service-worker.js
@@ -1,4 +1,4 @@
-importScripts("/roadside-forms/precache-manifest.615ea33d4e88f6af8cafed47a8a0f486.js", "/roadside-forms/workbox-v4.3.1/workbox-sw.js");
+importScripts("/roadside-forms/precache-manifest.bcc7487479d549ae653347193dfaca05.js", "/roadside-forms/workbox-v4.3.1/workbox-sw.js");
 workbox.setConfig({modulePathPrefix: "/roadside-forms/workbox-v4.3.1"});
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {
